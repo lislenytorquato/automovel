7032161a53386e9761f92d044c179dfe9bf91007
Lisleny Silva <lisleny.silva@dbserver.com.br>
2025-05-29 16:35:31 -0300

    First Commit

---
+++ src/main/java/questao18/automovel/AutomovelApplication.java
@@ -0,0 +1,13 @@
+package questao18.automovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/questao18/automovel/AutomovelApplicationTests.java
@@ -0,0 +1,13 @@
+package questao18.automovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
